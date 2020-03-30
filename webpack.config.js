1bccb4ea16e125d4470c6edc9b3daa70ec0dfaab
Matan Banner <matan.banner@gmail.com>
2020-03-30 19:34:41 +0300

    init + some basic client routing support

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+
+        }, {
+            test: /\.?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        
+        // tell the webserver to serve index.html in case of 404
+        // this is how we use client router
+        historyApiFallback: true
+    }
+}
