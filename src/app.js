ff0937320052f4bd66cf0005612a3e7da88c01b3
Matan Banner <matan.banner@gmail.com>
2020-03-30 19:44:06 +0300

    added support for 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 // standradise the view across all browsers 
 import 'normalize.css/normalize.css'
@@ -31,14 +31,23 @@ const HelpPage = () => (
     </div>
 )
 
+const NotFoundPage = () => (
+    <div>
+    404
+    </div>
+)
+
+
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
         
     </BrowserRouter>
 )
