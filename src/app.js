47aa9f97bc200220741405d567537e011b4c282b
Matan Banner <matan.banner@gmail.com>
2020-04-07 15:27:05 +0300

    connected redux to react

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -10,18 +11,30 @@ import './styles/styles.scss';
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill'}))
-store.dispatch(addExpense({ description: 'Gas bill'}))
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}))
+store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 1000}))
+store.dispatch(addExpense({ description: 'Water bill', amount: 109500}))
 
-store.dispatch(setTextFilter('bill'))
+
+
 // store.dispatch(setTextFilter('water'))
+
+// setTimeout(() => {
+//     store.dispatch(setTextFilter('bill'))    
+// }, 3000)
+
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses);
 
-
 console.log(store.getState())
 
+const jsx = (
+    <Provider store={store} >
+        <AppRouter />
+    </Provider>
+    
+)   
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
